d63371edf5c5a84e8ae227171a749bd5e0fee5f7
Nandini Rao [nandinirao] <nandinirao@costco.com>
2022-12-04 17:35:45 -0800

    class work exercises

--- constraint-validation.js
+++ constraint-validation.js
@@ -9,29 +9,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const firstName = document.getElementById('first-name');
     const lastName = document.getElementById('last-name');
     const email = document.getElementById('email');
-    const form = document.querySelector("form");
+    const form = document.getElementById('connect-form');
 
-    const checkStringLength = (maxLength, inputValue) => inputValue.length < maxLength;
-
-    const checkValidation = (e) => {
-        const allValid = false;
-        checkStringLength(3, firstName.value);
-        if (firstName.value.length < 3) {
-            firstName.validity.valid = false;
-            firstName.setCustomValidity(`Your name is too short`);
-            // need to report it to show the tooltip - for some reason it shows up on 2nd submit click if not put in
-            firstName.reportValidity();
-            localStorage.setItem('Invalid first name', 'true');
-        } else {
-            firstName.validity.valid = true;
-            allValid = true;
-        }
-
-        if(!allValid) {
-            e.preventDefault();
+    function checkValidation(e) {
+        let currentElement = e.target;
+        // console.log(`id:${currentElement.id}, value:${currentElement.value}, length:${currentElement.value.length}
+        // , valid:${currentElement.validity.valid}`);
+        
+        // without this the tool tip keeps showing!
+        currentElement.setCustomValidity("");
+        if (currentElement.id === 'first-name' ||
+            currentElement.id === 'last-name') {
+            if (!currentElement.validity.valid) {
+                currentElement.setCustomValidity(`Name has to be minimum of ${currentElement.minLength} letters`);
+                currentElement.reportValidity();
+            }
+        } else if (currentElement.id === 'email') {
+            if (!currentElement.validity.valid) {
+                console.log(currentElement.value);
+                currentElement.setCustomValidity(`Has to be a valid Email`);
+                currentElement.reportValidity();
+            }
         }
     }
 
-    //firstName.addEventListener('change', (e) => checkValidation(e));
-    form.addEventListener('submit', (e) => checkValidation(e));
+    firstName.addEventListener('input', checkValidation);
+    lastName.addEventListener('input', checkValidation);
+    email.addEventListener('input', checkValidation)
+
+    form.addEventListener('submit', (e) => {
+        if (!firstName.validity.valid || !lastName.validity.valid || !email.validity.valid) {
+            e.preventDefault();
+        }
+    });
 });
